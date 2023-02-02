48f25ba39f3e1f450fb246261424a3b7177bf67d
Dip Singha <dipsingha2@gmail.com>
2023-02-03 01:09:17 +0600

    description below hero section, added

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,5 +1,16 @@
 const Bio = () => {
-  return <div>Bio</div>;
+  return (
+    <div className="bio container mx-auto mt-20 overflow-hidden ">
+      <p>
+        A front-end developer skilled in React builds dynamic web app
+        interfaces. Proficiency in JavaScript and React is essential to create
+        reusable components and manage state. Understanding React's virtual DOM,
+        lifecycle methods, and hooks is key. Consider this field if you have a
+        passion for functional and visually appealing web experiences using
+        React.
+      </p>
+    </div>
+  );
 };
 
 export default Bio;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,10 @@
+import Bio from "./Bio";
 import Hero from "./Hero";
 const Home = () => {
   return (
     <div className="home" id="home">
       <Hero />
+      <Bio />
     </div>
   );
 };
