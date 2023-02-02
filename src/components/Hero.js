59aea7c8324c393cb90cab68e72b0a0bf5e4ad3d
Dip Singha <dipsingha2@gmail.com>
2023-02-03 00:19:22 +0600

    hero section created

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,15 +1,24 @@
 import { useRef } from "react";
+import { useHoverEffect } from "../hook/useHoverEffect";
+
+const data = {
+  image1:
+    "https://images.pexels.com/photos/2074108/pexels-photo-2074108.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  image2:
+    "https://images.pexels.com/photos/2955819/pexels-photo-2955819.jpeg?auto=compress&cs=tinysrgb&w=1600",
+};
 
 const Hero = () => {
   const heroImageRef = useRef(null);
+  useHoverEffect(heroImageRef, data.image1, data.image2);
 
   return (
-    <div className="hero container mx-auto mt-20">
+    <div className="hero container mx-auto mt-20 flex justify-center items-center">
       <div className="hero-img " ref={heroImageRef}></div>
-      <div className="shutter">
-        <h1>React JS</h1>
+      <div className="shutter shutter-left overflow-hidden">
+        <h1>Front-end</h1>
       </div>
-      <div className="shutter">
+      <div className="shutter shutter-right overflow-hidden">
         <h1>Developer</h1>
       </div>
       <div className="circle-left"></div>
