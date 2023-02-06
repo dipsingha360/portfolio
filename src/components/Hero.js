04b6da89e95f9a6167369a09e4ddd9d291e97923
Dip Singha <dipsingha2@gmail.com>
2023-02-07 02:25:25 +0600

    hero section gsap animation added

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,6 @@
 import { useRef } from "react";
 import { useHoverEffect } from "../hook/useHoverEffect";
+import { useImageReveal, useHeadlineReveal } from "../hook/gsap";
 
 const data = {
   image1:
@@ -10,16 +11,22 @@ const data = {
 
 const Hero = () => {
   const heroImageRef = useRef(null);
+  const heroHeadline1Ref = useRef(null);
+  const heroHeadline2Ref = useRef(null);
+
+  const headlines = [heroHeadline1Ref, heroHeadline2Ref];
   useHoverEffect(heroImageRef, data.image1, data.image2);
+  useImageReveal(heroImageRef, 0.5);
+  useHeadlineReveal(headlines, 0.7);
 
   return (
-    <div className="hero container mx-auto flex justify-center items-center">
+    <div className="hero container mx-auto flex justify-center items-center overflow-hidden">
       <div className="hero-img " ref={heroImageRef}></div>
       <div className="shutter shutter-left overflow-hidden">
-        <h1>Front-end</h1>
+        <h1 ref={heroHeadline1Ref}>Front-end</h1>
       </div>
       <div className="shutter shutter-right overflow-hidden">
-        <h1>Developer</h1>
+        <h1 ref={heroHeadline2Ref}>Developer</h1>
       </div>
       <div className="circle-left"></div>
       <div className="circle-right"></div>
---
+++ src/hook/gsap.js
@@ -0,0 +1,42 @@
+import { useEffect } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+export const useImageReveal = (el, delay = 0) => {
+  useEffect(() => {
+    gsap.fromTo(
+      el.current,
+      {
+        y: "-100vh",
+      },
+      {
+        y: 0,
+        duration: 2,
+        delay,
+        ease: "power4.out",
+      }
+    );
+  }, [el, delay]);
+};
+
+export const useHeadlineReveal = (items, delay = 0) => {
+  useEffect(() => {
+    const el = items.map((item) => item.current);
+
+    gsap.fromTo(
+      el,
+      {
+        y: "500",
+      },
+      {
+        y: 0,
+        duration: 1,
+        delay,
+        ease: "power4.out",
+        stagger: 0.2,
+      }
+    );
+  }, [items, delay]);
+};
